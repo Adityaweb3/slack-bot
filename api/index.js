718bd7aea2a727a563f85b968cb09c15f76badbc
Aditya Kumar Pandey <aditya@Adityas-MacBook-Pro.local>
2025-04-22 17:47:12 +0530

    slack bot

--- api/index.js
+++ api/index.js
@@ -1,19 +1,19 @@
+const express = require('express');
 const { WebClient } = require('@slack/web-api');
 const crypto = require('crypto');
-const express = require('express');
-const bodyParser = require('body-parser');
 
 const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
 const app = express();
 
-app.use(bodyParser.json());
+
+app.use('/api', express.raw({ type: 'application/json' }));
 
 const SHOP_ON_CALL_REGEX = /\bshop on[-\s]?call\b/i;
 
 app.post('/api', async (req, res) => {
   const slackSignature = req.headers['x-slack-signature'];
-  const requestBody = JSON.stringify(req.body);
   const timestamp = req.headers['x-slack-request-timestamp'];
+  const requestBody = req.body.toString('utf8');
 
   const hmac = crypto.createHmac('sha256', process.env.SLACK_SIGNING_SECRET);
   const [version, hash] = slackSignature.split('=');
@@ -24,12 +24,14 @@ app.post('/api', async (req, res) => {
     return res.status(400).send('Verification failed');
   }
 
-  if (req.body.type === 'url_verification') {
-    return res.send({ challenge: req.body.challenge });
+  const body = JSON.parse(requestBody);
+
+  if (body.type === 'url_verification') {
+    return res.status(200).send(body.challenge);
   }
 
-  if (req.body.event && req.body.event.type === 'app_mention') {
-    const text = req.body.event.text;
+  if (body.event && body.event.type === 'app_mention') {
+    const text = body.event.text;
 
     if (SHOP_ON_CALL_REGEX.test(text)) {
       await slackClient.chat.postMessage({
@@ -39,7 +41,7 @@ app.post('/api', async (req, res) => {
     }
   }
 
-  res.status(200).send('ok');
+  return res.status(200).send('ok');
 });
 
 module.exports = app;
